948b8ba90b0790c2db119d3476c310b2c9868a2f
ashmna <mnatsakanyan.ashot@gmail.com>
2016-05-19 13:26:54 +0400

    Implement runner start video watching algorithm.

--- src/main/java/org/proffart/grab/task/TaskRunner.java
+++ src/main/java/org/proffart/grab/task/TaskRunner.java
@@ -1,5 +1,6 @@
 package org.proffart.grab.task;
 
+import org.proffart.grab.Log;
 import org.proffart.grab.domains.Account;
 import org.proffart.grab.domains.Proxy;
 import org.proffart.grab.domains.Video;
@@ -17,6 +18,8 @@ public class TaskRunner {
     private List<Proxy> proxyList;
     private List<Account> accountList;
     private List<Video> videoList;
+    private int currentProxyIndex = -1;
+    private int currentAccountIndex = -1;
 
     public TaskRunner(List<Proxy> proxyList, List<Account> accountList, List<Video> videoList) {
         this.proxyList = proxyList;
@@ -25,11 +28,37 @@ public class TaskRunner {
     }
 
     public void start() {
+        double time = 0;
+        int accountCount;
 
+        while (!isStopWatching()) {
+            waitSecond((int) (time * 500));
+            accountCount = viewNum(time, accountList.size());
+            if (accountCount == 0 ) {
+                Log.instance.error("Vahan algoritmt chi ashxatum !!!");
+            }
+            while (--accountCount != 0) {
+                currentAccountIndex++;
+                watch(accountList.get(currentProxyIndex), getProxy(), videoList.size(), 0);
+            }
+            time += 0.2;
+        }
     }
 
-    public int viewNum(double time, int maxNum) {
-        return (int) (maxNum / Math.exp(time));
+    private boolean isStopWatching() {
+        return accountList.size() == 0 || currentAccountIndex == accountList.size();
+    }
+
+    private int viewNum(final double time, final int maxNum) {
+        return (int) (maxNum / 2 * Math.exp(time));
+    }
+
+    private Proxy getProxy() {
+        currentProxyIndex++;
+        if (currentProxyIndex == proxyList.size()) {
+            currentProxyIndex = 0;
+        }
+        return proxyList.get(currentProxyIndex);
     }
 
     private void watch(final Account account, final Proxy proxy, final int count, final int delaySecond) {
@@ -50,17 +79,20 @@ public class TaskRunner {
                 break;
             }
 
-            if (delaySecond <= 0) {
-                continue;
-            }
-
-            try {
-                Thread.sleep(delaySecond * 1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            waitSecond(delaySecond);
         }
         task.logout();
         task.closeBrowser();
+    }
+
+    private void waitSecond(int second) {
+        if (second <= 0) {
+            return;
+        }
+        try {
+            Thread.sleep(second * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
